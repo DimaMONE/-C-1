2f9220ef498856c7202b1ad7afd5da825a867301
SmaginDima <smaginzdima@gmail.com>
2022-11-27 18:01:10 +0300

    вторая задача

---
+++ exercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b && a > c) max = a;
+if (b > c) max = b;
+else max = c;
+Console.WriteLine($" Максимальное число равно {max} ");
+
+
+
